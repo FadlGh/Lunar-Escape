e2d17fd648eb53aa38fe672752b891a18239f4ff
FadlGh <fadl2009gh@gmail.com>
2023-07-28 14:11:26 +0300

    Door #2

--- Assets/Scripts/Base/Door.cs
+++ Assets/Scripts/Base/Door.cs
@@ -1,18 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator am;
+
     void Start()
     {
-        
+        am = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        am.SetBool("Open", true);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        am.SetBool("Open", false);
     }
 }
