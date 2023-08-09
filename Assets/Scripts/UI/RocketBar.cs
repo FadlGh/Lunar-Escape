37952099d08f466012814504f3f3cd5162536012
FadlGh <fadl2009gh@gmail.com>
2023-08-09 12:43:09 +0300

    Rocket bar upgrade system #1 #5

--- Assets/Scripts/UI/RocketBar.cs
+++ Assets/Scripts/UI/RocketBar.cs
@@ -1,14 +1,39 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RocketBar : MonoBehaviour
 {
     [SerializeField] private Animator bar;
+    [SerializeField] private Slider slider;
+
+    Dictionary<string, int> requiredItems = new Dictionary<string, int>
+        {
+            { "Moon Rock", 2 },
+            { "Energy", 2 },
+            { "Metal", 2 }
+        };
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        Upgrade();
         bar.SetBool("Open", true);
+    }
+    private void Upgrade()
+    {
+        if (InventoryManager.Instance.CheckItemsAvailability(requiredItems))
+        {
+            slider.value += 20f;
+            foreach (var kvp in requiredItems)
+            {
+                InventoryManager.Instance.RemoveItem(kvp.Key, kvp.Value);
+            }
+            Upgrade();
+        }
+        else
+        {
+            return;
+        }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
